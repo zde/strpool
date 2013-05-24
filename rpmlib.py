1b642b44b9b9d0046e37449b9ebbd8dda0ad9f7c
Zdenek Pavlas <zpavlas@redhat.com>
2013-05-24 14:57:27 +0200

    .

--- rpmlib.py
+++ rpmlib.py
@@ -2,36 +2,59 @@
 import bsddb
 from struct import unpack
 
-def strtab(hdr, off, cnt):
-    while cnt:
-        off2 = hdr.index('\0', off)
-        yield hdr[off:off2]
-        cnt -= 1; off = off2
-
 class Package(object):
-    __slots__ = 'name version epoch arch'.split()
-    use = set((1000, 1001, 1002, 1003, 1022))
+    __slots__ = 'name version arch provides requires'.split()
+    use = set((
+        1000, 1001, 1002, 1003, 1022, # nvrea
+        1047, 1112, 1113, # provides
+        1049, 1048, 1050, # requires
+    ))
 
     def __init__(self, hdr, i=0):
         n, s = unpack('>2I', hdr[i:i + 8])
-        i += 8; b = i + n*16; d = {}
+        i += 8; b = i + n*16
+        d = {}
+        print 'NEW'
+        prev = 0
         while n:
             tag, t, o, c = unpack('>4I', hdr[i:i + 16])
             i += 16; n -= 1
+            print 'TAG', tag
+            assert tag > prev, (prev, tag); prev = tag
             if tag not in self.use:
                 continue
             o += b
             v = []
             while c:
-                if t == 6: p = hdr.index('\0', o); v.append(hdr[o:p]); o = p + 1
-                elif t == 4: v.append(unpack('>I', hdr[o:o + 4])[0]); o += 4
+                if t == 6 or t == 8:
+                    p = hdr.index('\0', o)
+                    v.append(hdr[o:p])
+                    o = p + 1
+                elif t == 4:
+                    v.append(unpack('>I', hdr[o:o + 4])[0])
+                    o += 4
                 else: raise ValueError, (tag, t)
                 c -= 1
             d[tag] = v
+
+        # nevra
         self.name = d[1000][0]
-        self.version = d[1001][0] +'-'+ d[1002][0]
-        self.epoch = d.get(1003, [None])[0]
-        self.arch  = d.get(1022, [None])[0]
+        epoch = d.get(1003, (None,))[0]
+        epoch = epoch and '%d:' % epoch or ''
+        self.version = '%s%s-%s' % (epoch, d[1001][0], d[1002][0])
+        self.arch  = d.get(1022, (None,))[0]
+
+        # prco
+        xlat = None, 1, 4, 5, 2, 3, 6, None
+        def decode(flag, version):
+            flag = xlat[flag >> 1 & 7]
+            if flag: flag = flag, version
+            return flag
+        def prco(*tags):
+            return [(name, decode(flag, version))
+                    for name, flag, version in zip(*[d.get(t, ()) for t in tags])]
+        self.provides = prco(1047, 1112, 1113)
+        self.requires = prco(1049, 1048, 1050)
 
     def __str__(self):
         if not self.arch:
@@ -53,3 +76,5 @@ def installed(name='/var/lib/rpm/Packages'):
 if __name__ == '__main__':
     for pkg in installed():
         print pkg
+        for p in pkg.requires:
+            print '\t', p
